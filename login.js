998005b0c6d8021c0acbc12b6beba9454b225590
SidD <sidharthdamoddharn@outlook.com~>
2023-05-29 00:36:31 -0600

    Jeman Kahati

---
+++ login.js
@@ -0,0 +1,23 @@
+const loginButton = document.getElementById("login-button");
+
+loginButton.addEventListener("click", function(event) {
+  event.preventDefault(); // prevent form submission
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // do some validation
+  if (username === "admin" && password === "password") {
+    alert("Login successful");
+    window.location.href = "dashboard.html";
+  } 
+  else {
+    alert("Invalid username or password");
+  }
+
+  if(username === "pheonix"&& password === "yo"){
+    alert("Login Successful");
+    window.location.href = "dashboard.html";
+  }
+});
+
